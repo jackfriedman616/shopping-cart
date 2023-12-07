3ff3812ab806d017ff151facdc8f4fe7dc1ab76d
jackfriedman616 <124191379+jackfriedman616@users.noreply.github.com>
2023-12-07 09:25:19 -0500

    Replaced placeholder image for home page.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import { transformSync } from '@babel/core';
 const Home = () => {
   return (
     <div className='home-container' style={{
-      backgroundImage: `url(${process.env.PUBLIC_URL + 'https://www.shutterstock.com/image-vector/chattogram-bangladesh-april-24-2023-260nw-2292992631.jpg'})`, //replace placeholder image file
+      backgroundImage: `url(${process.env.PUBLIC_URL + 'https://60a99bedadae98078522-a9b6cded92292ef3bace063619038eb1.ssl.cf2.rackcdn.com/images_stores_Cambridge_CambridgeSLIDER_laptop.jpg'})`, 
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
       backgroundPosition: 'bottom'
