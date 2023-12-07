542b87da3cd9b35b815f40b7ad2ba21df019df78
jackfriedman616 <124191379+jackfriedman616@users.noreply.github.com>
2023-12-07 09:29:56 -0500

    Changed home page background

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import { transformSync } from '@babel/core';
 const Home = () => {
   return (
     <div className='home-container' style={{
-      backgroundImage: `url(${process.env.PUBLIC_URL + 'https://60a99bedadae98078522-a9b6cded92292ef3bace063619038eb1.ssl.cf2.rackcdn.com/images_stores_Cambridge_CambridgeSLIDER_laptop.jpg'})`, 
+      backgroundImage: `url(${process.env.PUBLIC_URL + '"C:\Users\jackf\OneDrive\Pictures\Saved Pictures\space.jpg"'})`, 
       backgroundRepeat: 'no-repeat',
       backgroundSize: 'cover',
       backgroundPosition: 'bottom'
